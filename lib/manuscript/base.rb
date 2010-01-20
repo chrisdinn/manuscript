dbfd5a3efaeb381d127c0ef05fcfbcdb1db719d6
chrisdinn <chrisgdinn@gmail.com>
2010-01-20 11:43:25 -0500

    Fix bug to make current_user available on index page

--- lib/manuscript/base.rb
+++ lib/manuscript/base.rb
@@ -8,6 +8,7 @@ module Manuscript
     end
     
     get "/" do
+      @user = session[:sso].nil? ? nil : User.new(session[:sso])
       @page = Page.find_by_name('index')
       raise Sinatra::NotFound unless @page
       @page.to_html(@user)
